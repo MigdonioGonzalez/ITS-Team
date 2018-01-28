994021b61157025552b6499939ebdd89ab1f8547
MigdonioGonzalez <migdonio.gonzalez@utp.ac.pa>
2018-01-28 15:07:19 -0500

    V1.2 AGREGAR SHOWPREP

--- GetPlacementReport.R
+++ GetPlacementReport.R
@@ -1,23 +1,27 @@
-GetPlacementReport <- function(inicial = "09-01-2017",final = "12-31-2017"){
-  username <- "USERNAME"
+GetPlacementReport <- function(inicial = "",final = ""){
+  username <- ""
   username <- paste0("username=",username)
   
-  password <- "PASSWORD"
+  password <- ""
   password <- paste0("password=",password)
   
-  class_code <- "CLASSCODE"
+  class_code <- ""
   class_code <- paste0("class_code=",class_code)
   
   inicial <- paste0("from_date=",inicial)
   final <- paste0 ("to_date=", final)
   
-  type <- "last"
+  prep <- paste0("show_prep=true")
+  
+  type <- "all"
   type <- paste0("type=",type)
   
   URL <- "https://api.aleks.com/urlrpc?method=getPlacementReport"
-  URL <- paste(URL,username,password,class_code,inicial,final,type,sep="&")
+  URL <- paste(URL,username,password,class_code,inicial,final,type,prep,sep="&")
   URL <- paste0(URL,"&page_num=1")
   
-  data_estudiantes <<- read.csv(URL,encoding = "UTF-8")
+  data_estudiantes <- read.csv(URL,encoding = "UTF-8")
   View(data_estudiantes)
+  
+  return(data_estudiantes)
 }
